447c7c41461660a1de434a47cc5e09cf7da68207
kenkenissocool <s2010246@s.tsukuba.ac.jp>
2022-05-27 17:27:22 +0900

    沖縄追加&テキスト表示

--- calc.js
+++ calc.js
@@ -69,6 +69,13 @@ function staticLoadPlaces() {
                 lng: 139.9409352,
             }
         },
+        {
+            name: 'Okinawa',
+            location: {
+                lat: 26.7306889,
+                lng: 128.2216491,
+            }
+        },
 
     ];
 }
@@ -87,8 +94,9 @@ function renderPlaces(places, pos) {
 	console.log(`heading: ${ crd.heading }`);
         cal.calcNewPosition(cal.currentPosition, cal.bearing, cal.newDistance);
         cal.calcSizeDist(cal.distance);
-        let model = document.createElement('a-box');
-        model.setAttribute('material', 'color: red');
+        let model = document.createElement('a-text');
+        model.setAttribute('value', `${name}`);
+        model.setAttribute('look-at', '[gps-camera]');
         model.setAttribute('gps-entity-place', `latitude: ${cal.newPosition[0]}; longitude: ${cal.newPosition[1]};`);
         model.setAttribute('scale', `${cal.objectSize}`);
 
