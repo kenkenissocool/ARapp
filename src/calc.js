e8681a6a616b221f73abc97b0ce29668085421e1
kenkenissocool <s2010246@s.tsukuba.ac.jp>
2022-06-15 13:37:59 +0900

    fix: change box size

--- src/calc.js
+++ src/calc.js
@@ -39,7 +39,7 @@ export class CalcVR {
     this.newPosition = [calculatedlced.latitude, calculatedlced.longitude];
   }
   calcSizeDist(distance) {
-    if (distance <= 1000 && distance >= 0) {
+    if (distance <= 1000 && distance >= 500) {
       this.objectSize = "50 50 50";
       this.newDistance = 800;
     } else if (distance > 1000 && distance <= 8000) {
@@ -57,7 +57,7 @@ export class CalcVR {
     }
   }
 }
-
+a
 let coordinates = [];
 
 window.onload = async () => {
@@ -114,12 +114,11 @@ function renderPlaces(places, pos) {
 
     let model2 = document.createElement("a-box");
     model2.setAttribute("material", `color:red`);
-    model2.setAttribute("look-at", "[gps-camera]");
     model2.setAttribute(
       "gps-entity-place",
-      `latitude: ${cal.splitsLat[0]}; longitude: ${cal.splitsLon[0]};`
+      `latitude: ${cal.splitsLat[3]}; longitude: ${cal.splitsLon[3]};`
     );
-    model2.setAttribute("scale", `${cal.objectSize}`);
+    model2.setAttribute("scale", "50 50 50");
     model2.addEventListener("loaded", () => {
       window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
     });
