34ddf245d5bf43505d9377f9f23488251267d000
kenkenissocool <s2010246@s.tsukuba.ac.jp>
2022-06-24 17:26:15 +0900

    マイナスに変えた

--- src/calc.js
+++ src/calc.js
@@ -39,11 +39,11 @@ export class CalcVR {
 
     this.splitsLat = [];
     this.splitsLon = [];
-    this.distanceLat = currentPosition[0]-this.newPosition[0]/20;
-    this.distanceLon = currentPosition[1]-this.newPosition[1]/20;
+    this.distanceLat = Math.abs(currentPosition[0]-this.newPosition[0])/20;
+    this.distanceLon = Math.abs(currentPosition[1]-this.newPosition[1])/20;
     for (let t = 0; t < 20; t++) {
-      this.splitsLat.push(currentPosition[0] + this.distanceLat*t);
-      this.splitsLon.push(currentPosition[1] + this.distanceLon*t);
+      this.splitsLat.push(currentPosition[0] - this.distanceLat*t);
+      this.splitsLon.push(currentPosition[1] - this.distanceLon*t);
     }
   }
   calcSizeDist(distance) {
