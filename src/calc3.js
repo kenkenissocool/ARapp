7043d3e1ec59e32d1e896d62d60f35218603ce45
kenkenissocool <s2010246@s.tsukuba.ac.jp>
2022-07-24 16:01:19 +0900

    rotate

--- src/calc3.js
+++ src/calc3.js
@@ -158,6 +158,7 @@ function renderPlaces(places, pos) {
         "gps-entity-place",
         `latitude: ${cal.splitsLat[i]}; longitude: ${cal.splitsLon[i]};`
       );
+      model2.setAttribute("rotation", "-90 0 0");
       model2.setAttribute("scale", `${1} ${1} ${1}`);
       model2.addEventListener("loaded", () => {
         window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
